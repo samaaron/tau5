323669c8cf84d6d2dc30d258c2a06475df6b47b1
Sam Aaron <samaaron@gmail.com>
2024-09-25 15:20:56 +0100

    Build - fix mac build all script

--- bin/mac/build.sh
+++ bin/mac/build.sh
@@ -11,7 +11,7 @@ cleanup_function() {
 }
 trap cleanup_function EXIT
 
-# "${SCRIPT_DIR}"/build-server.sh
+"${SCRIPT_DIR}"/build-server.sh
 "${SCRIPT_DIR}"/build-app.sh
 "${SCRIPT_DIR}"/build-release.sh
 
