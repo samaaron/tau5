cf96d48ab919766e2ee875a394be6f771f396bd7
Sam Aaron <samaaron@gmail.com>
2024-10-16 00:07:52 +0100

    build - add clean batch file for Windows

---
+++ bin/win/clean.bat
@@ -0,0 +1,17 @@
+set WORKING_DIR=%CD%
+cd %~dp0
+
+cd ..\..\
+cd server
+cmd /c mix clean
+rmdir _build /s /q
+rmdir deps /s /q
+del priv\static\assets\*.* /s /q
+for /d %%d in (priv\static\assets\*) do rmdir "%%d" /s /q
+
+cd ..\
+rmdir app\build /s /q
+rmdir Release /s /q
+rmdir build /s /q
+
+cd %WORKING_DIR%
