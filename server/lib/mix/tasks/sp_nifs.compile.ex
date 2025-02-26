494a8ab63d7b8040485e6c411cee548895f01c70
Sam Aaron <samaaron@gmail.com>
2025-02-26 14:47:37 +0000

    NIFs - enable building on arm64 linux

--- server/lib/mix/tasks/sp_nifs.compile.ex
+++ server/lib/mix/tasks/sp_nifs.compile.ex
@@ -21,21 +21,23 @@ defmodule Mix.Tasks.SpNifs.Compile do
     compile_splink(platform, arch)
   end
 
-  defp compile_win_x64(proj) do
-    Logger.info("Compiling #{proj} for Windows x64")
-    File.mkdir_p("deps/#{proj}/build")
+  defp cmake_build_and_install(
+         build_dir: build_dir,
+         install_dir: install_dir,
+         cmake_args: cmake_args
+       ) do
+    File.mkdir_p(build_dir)
 
-    File.cd!("deps/#{proj}/build", fn ->
+    cmake_args =
+      [
+        "-DCMAKE_INSTALL_PREFIX=#{install_dir}",
+        "-DCMAKE_BUILD_TYPE=Release",
+        ".."
+      ] ++ cmake_args
+
+    File.cd!(build_dir, fn ->
       {cmake_output, cmake_status} =
-        System.cmd("cmake", [
-          "-G",
-          "Visual Studio 17 2022",
-          "-A",
-          "x64",
-          "-DCMAKE_INSTALL_PREFIX=../../../priv/nif",
-          "-DCMAKE_BUILD_TYPE=Release",
-          ".."
-        ])
+        System.cmd("cmake", cmake_args)
 
       Logger.debug("CMake output:\n#{cmake_output}")
       if cmake_status != 0, do: raise("CMake failed with status #{cmake_status}")
@@ -52,43 +54,69 @@ defmodule Mix.Tasks.SpNifs.Compile do
       Logger.debug("CMake output:\n#{cmake_output}")
       if cmake_status != 0, do: raise("CMake failed with status #{cmake_status}")
 
-      Logger.info("Building #{proj} for Win x64 complete")
+      Logger.info("Building #{build_dir} complete")
     end)
+  end
+
+  defp compile_lin_arm64(proj) do
+    Logger.info("Compiling #{proj} for Linux arm64")
+    proj_base_dir = "deps/#{proj}"
+    proj_build_dir = "#{proj_base_dir}/build"
+    File.mkdir_p(proj_build_dir)
+
+    cmake_build_and_install(
+      build_dir: proj_build_dir,
+      install_dir: "#{proj_base_dir}/priv/nif",
+      cmake_args: [
+        "-G",
+        "Unix Makefiles",
+        "-DCMAKE_OSC_ARCHITECTURES=ARM64"
+      ]
+    )
+  end
+
+  defp compile_win_x64(proj) do
+    Logger.info("Compiling #{proj} for Windows x64")
+    proj_base_dir = "deps/#{proj}"
+    proj_build_dir = "#{proj_base_dir}/build"
+    File.mkdir_p(proj_build_dir)
+
+    cmake_build_and_install(
+      build_dir: proj_build_dir,
+      install_dir: "#{proj_base_dir}/priv/nif",
+      cmake_args: [
+        "-G",
+        "Visual Studio 17 2022",
+        "-A",
+        "x64"
+      ]
+    )
+
+    Logger.info("Building #{proj} for Win x64 complete")
   end
 
   defp compile_mac_arm64(proj) do
     Logger.info("Compiling #{proj} for macOS arm64")
-    File.mkdir_p("deps/#{proj}/build")
+    proj_base_dir = "deps/#{proj}"
+    proj_build_dir = "#{proj_base_dir}/build"
+    File.mkdir_p(proj_build_dir)
 
-    File.cd!("deps/#{proj}/build", fn ->
-      {cmake_output, cmake_status} =
-        System.cmd("cmake", [
-          "-G",
-          "Unix Makefiles",
-          "-DCMAKE_OSC_ARCHITECTURES=ARM64",
-          "-DCMAKE_INSTALL_PREFIX=../../../priv/nif",
-          "-DCMAKE_OSX_DEPLOYMENT_TARGET=12",
-          "-DCMAKE_BUILD_TYPE=Release",
-          ".."
-        ])
+    cmake_build_and_install(
+      build_dir: proj_build_dir,
+      install_dir: "#{proj_base_dir}/priv/nif",
+      cmake_args: [
+        "-G",
+        "Unix Makefiles",
+        "-DCMAKE_OSC_ARCHITECTURES=ARM64",
+        "-DCMAKE_OSX_DEPLOYMENT_TARGET=12"
+      ]
+    )
 
-      Logger.debug("CMake output:\n#{cmake_output}")
-      if cmake_status != 0, do: raise("CMake failed with status #{cmake_status}")
+    Logger.info("Building #{proj} for macOS arm64 complete")
+  end
 
-      {cmake_output, cmake_status} =
-        System.cmd("cmake", ["--build", ".", "--config", "Release"])
-
-      Logger.debug("CMake output:\n#{cmake_output}")
-      if cmake_status != 0, do: raise("CMake failed with status #{cmake_status}")
-
-      {cmake_output, cmake_status} =
-        System.cmd("cmake", ["--install", "."])
-
-      Logger.debug("CMake output:\n#{cmake_output}")
-      if cmake_status != 0, do: raise("CMake failed with status #{cmake_status}")
-
-      Logger.info("Building #{proj} for macOS arm64 complete")
-    end)
+  defp compile_spmidi(:linux, :arm64) do
+    compile_lin_arm64("sp_midi")
   end
 
   defp compile_spmidi(:win, :x64) do
@@ -101,6 +129,10 @@ defmodule Mix.Tasks.SpNifs.Compile do
 
   defp compile_spmidi(os, arch) do
     Logger.info("Uknown OS or architecture to compile spmidi for: #{inspect([os, arch])}")
+  end
+
+  defp compile_splink(:linux, :arm64) do
+    compile_lin_arm64("sp_link")
   end
 
   defp compile_splink(:win, :x64) do
