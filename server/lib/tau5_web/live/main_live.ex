abfbf20548479f38db6fe3ffb24878fe2dcda586
Sam Aaron <samaaron@gmail.com>
2025-06-10 11:18:37 +0100

    GUI - remove old discovery code

--- server/lib/tau5_web/live/main_live.ex
+++ server/lib/tau5_web/live/main_live.ex
@@ -3,14 +3,12 @@ defmodule Tau5Web.MainLive do
   require Logger
   require Tau5.MonacoEditor
 
-  @known_nodes_topic "known_nodes"
   @link_num_peers_topic "link-num-peers"
   @link_tempo_topic "link-tempo"
 
   @impl true
   def mount(_params, _session, socket) do
     if connected?(socket) do
-      Phoenix.PubSub.subscribe(Tau5.PubSub, @known_nodes_topic)
       Phoenix.PubSub.subscribe(Tau5.PubSub, @link_num_peers_topic)
       Phoenix.PubSub.subscribe(Tau5.PubSub, @link_tempo_topic)
     end
@@ -19,7 +17,6 @@ defmodule Tau5Web.MainLive do
      assign(socket,
        code:
          "// Tau5 Editor\n\nosc(10, 0.1, 0.8)\n  .rotate(0.1)\n  .modulate(osc(10), 0.5)\n  .out()",
-       nodes: Tau5.Discovery.nodes(),
        link_tempo: Tau5.Link.tempo(),
        link_num_peers: Tau5.Link.num_peers()
      )}
@@ -36,7 +33,7 @@ defmodule Tau5Web.MainLive do
         <h1 class="inline-block p-1 text-xl text-black text-opacity-75 bg-white rounded-sm mix-blend-difference bg-opacity-70">
           Welcome to Tau5
         </h1>
-        
+
         <p class="pb-5 text-xl text-white mix-blend-difference">Code. Art. Live.</p>
       </div>
        <img class="mix-blend-difference" src="/images/tau5-bw.png" alt="Tau5 Logo" width="50" />
@@ -50,11 +47,11 @@ defmodule Tau5Web.MainLive do
         >
           <.icon name="hero-play-circle" class="w-8 h-8 text-white hover:text-orange-400" />
         </div>
-        
+
         <div class="inline-block bg-black rounded-sm bg-opacity-70 mix-blend-difference hover:mix-blend-normal">
           <.icon name="hero-stop" class="w-8 h-8 text-white hover:text-orange-400" />
         </div>
-        
+
         <div class="inline-block bg-black rounded-sm bg-opacity-70 mix-blend-difference hover:mix-blend-normal">
           <.icon
             name="hero-arrow-path-rounded-square"
@@ -62,7 +59,7 @@ defmodule Tau5Web.MainLive do
           />
         </div>
       </div>
-      
+
       <div
         id={@editor_id}
         phx-hook="Tau5EditorHook"
@@ -76,43 +73,18 @@ defmodule Tau5Web.MainLive do
         <canvas hydra class="absolute top-0 left-0 z-0 w-full h-full"></canvas>
         <div class="relative top-0 left-0 z-10 w-full h-full" id={@monaco_id} monaco-code-editor>
         </div>
-        
+
         <div resize-handle class="absolute bottom-0 right-0 z-20 w-4 h-4 cursor-se-resize"></div>
       </div>
-      
+
       <div class="p-1 m-1 text-xl text-black text-opacity-75 bg-white rounded-sm mix-blend-difference bg-opacity-70">
         <span>
           [Ableton Link - tempo: {Float.round(@link_tempo, 2)},
         </span>
-        
+
         <span>
           peers: {@link_num_peers}]
         </span>
-      </div>
-      
-      <div class="text-white bg-black bg-opacity-70 mix-blend-difference">
-        <p>
-          Tau5 Nodes:
-          <ul>
-            <%= for {[_discovery_interface, hostname, ip, _uuid], [info, _last_seen, _transient]} <- @nodes do %>
-              <li class="grid items-end grid-cols-2 gap-1 ">
-                <div class="p-1 m-1 text-xl text-black text-opacity-75 bg-white rounded-sm mix-blend-difference bg-opacity-70">
-                  <span>
-                    {hostname}
-                  </span>
-                </div>
-                
-                <div class="p-1 m-1 text-xl text-black text-opacity-75 bg-white rounded-sm mix-blend-difference bg-opacity-70">
-                  <a href={ip_and_port_to_http_string(ip, info["http_port"])}>
-                    <span>
-                      {IP.to_string(ip)}:{info["http_port"]}
-                    </span>
-                  </a>
-                </div>
-              </li>
-            <% end %>
-          </ul>
-        </p>
       </div>
     </div>
     """
@@ -139,9 +111,6 @@ defmodule Tau5Web.MainLive do
   end
 
   @impl true
-  def handle_info({:nodes_updated, nodes}, socket) do
-    {:noreply, assign(socket, :nodes, nodes)}
-  end
 
   def handle_info({:link_num_peers, num_peers}, socket) do
     {:noreply, assign(socket, :link_num_peers, num_peers)}
