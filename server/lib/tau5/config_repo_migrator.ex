b7b76446bfac40a35acf3862fe82617ef176fbea
Sam Aaron <samaaron@gmail.com>
2025-06-17 10:55:27 +0100

    Server - make config repo migrations synchronous

--- server/lib/tau5/config_repo_migrator.ex
+++ server/lib/tau5/config_repo_migrator.ex
@@ -1,14 +1,22 @@
 defmodule Tau5.ConfigRepoMigrator do
-  use Task, restart: :transient
+  def child_spec(_) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :run, []},
+      type: :worker,
+      restart: :transient
+    }
+  end
 
-  def start_link(_arg) do
-    Task.start_link(fn ->
-      path = Tau5.Paths.config_repo_migrations_path()
+  # called by the supervisor
+  def run do
+    path = Tau5.Paths.config_repo_migrations_path()
 
-      Ecto.Migrator.with_repo(
-        Tau5.ConfigRepo,
-        &Ecto.Migrator.run(&1, path, :up, all: true)
-      )
-    end)
+    Ecto.Migrator.with_repo(
+      Tau5.ConfigRepo,
+      &Ecto.Migrator.run(&1, path, :up, all: true)
+    )
+
+    :ignore
   end
 end
