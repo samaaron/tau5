dc45b3bce3196a82752d06ecaa9d592273949ee3
Sam Aaron <samaaron@gmail.com>
2025-07-01 22:07:37 +0100

    Build - add tau5 start script for windows

---
+++ bin/win/tau5.bat
@@ -0,0 +1,12 @@
+@echo off
+setlocal
+
+set SCRIPT_DIR=%~dp0
+set WORKING_DIR=%CD%
+set ROOT_DIR=%SCRIPT_DIR%..\..
+
+cd /d "%ROOT_DIR%"
+app\build\Release\Tau5.exe dev
+
+cd /d "%WORKING_DIR%"
+endlocal
