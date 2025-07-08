a2809ca74d863f7a4762814f0bd45a3dba3ae0e9
Sam Aaron <samaaron@gmail.com>
2025-07-08 10:45:15 +0100

    Config - don't start midi, link and discovery services during test run

--- server/config/test.exs
+++ server/config/test.exs
@@ -1,5 +1,9 @@
 import Config
 
+config :tau5, :midi_enabled, false
+config :tau5, :link_enabled, false
+config :tau5, :discovery_enabled, false
+
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
 config :tau5, Tau5Web.Endpoint,
