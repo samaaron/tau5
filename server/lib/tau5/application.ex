8b8630b5118563f55c33a168d957729b232adcc0
Sam Aaron <samaaron@gmail.com>
2025-07-08 12:09:31 +0100

    Server - run migrations more synchronously

--- server/lib/tau5/application.ex
+++ server/lib/tau5/application.ex
@@ -1,60 +1,52 @@
 defmodule Tau5.Application do
-  # See https://hexdocs.pm/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
   require Logger
 
   @impl true
   def start(_type, _args) do
+    # Start ConfigRepo first
+    {:ok, _} =
+      Supervisor.start_link([Tau5.ConfigRepo],
+        strategy: :one_for_one,
+        name: Tau5.ConfigRepoSupervisor
+      )
+
+    # Run migrations synchronously
+    run_migrations!()
+
+    # Now start everything else
     http_port = Application.get_env(:tau5, Tau5Web.Endpoint)[:http][:port]
 
-    midi_enabled = Application.get_env(:tau5, :midi_enabled, false)
-    link_enabled = Application.get_env(:tau5, :link_enabled, false)
-    discovery_enabled = Application.get_env(:tau5, :discovery_enabled, false)
+    children = [
+      # ConfigRepoMigrator removed from here!
+      Tau5Web.Telemetry,
+      {Phoenix.PubSub, name: Tau5.PubSub},
+      {Finch, name: Tau5.Finch},
+      Tau5Web.Endpoint,
+      Tau5.Link,
+      Tau5.MIDI,
+      {Tau5.Discovery, %{http_port: http_port}}
+    ]
 
-    if midi_enabled do
-      Logger.info("Initialising MIDI native interface")
-      :sp_midi.init()
-    else
-      Logger.info("Starting without MIDI native interface")
-    end
-
-    if link_enabled do
-      Logger.info("Initialising Link native interface")
-      :sp_link.init()
-    else
-      Logger.info("Starting without Link native interface")
-    end
-
-    if discovery_enabled do
-      Logger.info("Initialising Discovery native interface")
-      :tau5_discovery.init()
-    else
-      Logger.info("Starting without Discovery native interface")
-    end
-
-    children =
-      [
-        Tau5.ConfigRepo,
-        Tau5.ConfigRepoMigrator,
-        Tau5Web.Telemetry,
-        {Phoenix.PubSub, name: Tau5.PubSub},
-        {Finch, name: Tau5.Finch},
-        Tau5Web.Endpoint,
-        Tau5.Link,
-        {Tau5.Discovery, %{http_port: http_port}}
-      ]
-
-    # See https://hexdocs.pm/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Tau5.Supervisor]
     Supervisor.start_link(children, opts)
   end
 
-  # Tell Phoenix to update the endpoint configuration
-  # whenever the application is updated.
+  defp run_migrations! do
+    path = Tau5.Paths.config_repo_migrations_path()
+
+    case Ecto.Migrator.with_repo(
+           Tau5.ConfigRepo,
+           &Ecto.Migrator.run(&1, path, :up, all: true)
+         ) do
+      {:ok, _apps, num} ->
+        Logger.info("Ran #{num} ConfigRepo migrations")
+
+      {:error, error} ->
+        raise "Failed to run migrations: #{inspect(error)}"
+    end
+  end
+
   @impl true
   def config_change(changed, _new, removed) do
     Tau5Web.Endpoint.config_change(changed, removed)
