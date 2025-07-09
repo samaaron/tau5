d964b30bc41ea4c2da5818a86e82489738300a2a
Sam Aaron <samaaron@gmail.com>
2025-07-09 12:16:54 +0100

    Build - improve Windows clean script logging

--- bin/win/clean.bat
+++ bin/win/clean.bat
@@ -1,24 +1,87 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Jump to main script, skipping function definitions
+goto :main
+
+:: ========== FUNCTIONS ==========
+
+:cleanDir
+:: Function to clean a directory
+:: Parameter: %~1 = directory path
+if exist "%~1" (
+    echo   - Removing %~1
+    rmdir "%~1" /s /q
+) else (
+    echo   - %~1 [not found]
+)
+goto :eof
+
+:cleanAssets
+:: Function to clean assets directory (files and subdirectories)
+:: Parameter: %~1 = assets directory path
+if exist "%~1" (
+    echo   - Cleaning %~1
+    :: Delete all files
+    del "%~1\*.*" /s /q 2>nul
+    :: Delete all subdirectories
+    for /d %%d in ("%~1\*") do (
+        echo     - Removing directory %%~nxd
+        rmdir "%%d" /s /q
+    )
+) else (
+    echo   - %~1 [not found]
+)
+goto :eof
+
+:: ========== MAIN SCRIPT ==========
+
+:main
 set WORKING_DIR=%CD%
 cd %~dp0
+cd ..\..
 
-cd ..\..\
-cd server
-rmdir deps\sp_link/build /s /q
-rmdir deps\sp_link\deps\sp_link /s /q
-rmdir deps\tau5_discovery\build /s /q
-rmdir deps\exqlite\build /s /q
-rmdir priv\nifs /s /q
-mkdir priv\nifs
+echo Starting cleanup process...
+echo.
 
-rmdir _build /s /q
-rmdir deps /s /q
-del priv\static\assets\*.* /s /q
-for /d %%d in (priv\static\assets\*) do rmdir "%%d" /s /q
-cd ..\
-rmdir gui\build /s /q
-rmdir Release /s /q
-rmdir build /s /q
+:: Server directory cleanup
+cd server 2>nul
+if !errorlevel! equ 0 (
+    echo Cleaning server directory...
+
+    :: Clean dependencies
+    call :cleanDir "deps\sp_link\build"
+    call :cleanDir "deps\sp_link\deps\sp_link"
+    call :cleanDir "deps\tau5_discovery\build"
+    call :cleanDir "deps\exqlite\build"
+
+    :: Clean and recreate priv\nifs
+    call :cleanDir "priv\nifs"
+    echo   - Creating priv\nifs
+    mkdir priv\nifs 2>nul
+
+    :: Clean build directories
+    call :cleanDir "_build"
+    call :cleanDir "deps"
+
+    :: Clean static assets
+    call :cleanAssets "priv\static\assets"
+
+    cd ..
+) else (
+    echo Server directory not found, skipping server cleanup...
+)
+
+echo.
+echo Cleaning root directories...
+
+:: Clean root level directories
+call :cleanDir "gui\build"
+call :cleanDir "Release"
+call :cleanDir "build"
 
 cd %WORKING_DIR%
 
-echo Cleaned up any build directories that were found.
+echo.
+echo Cleanup process completed successfully.
+endlocal
