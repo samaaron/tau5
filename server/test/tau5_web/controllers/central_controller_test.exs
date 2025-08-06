c0d034da8fd62a84ddd5d80d5d2ec24d8f045dda
Sam Aaron <samaaron@gmail.com>
2025-08-06 14:38:41 +0100

    Server - fix tests for landing page

---
+++ server/test/tau5_web/controllers/central_controller_test.exs
@@ -0,0 +1,28 @@
+defmodule Tau5Web.CentralControllerTest do
+  use Tau5Web.ConnCase
+
+  describe "index" do
+    test "redirects to /app when not in central mode", %{conn: conn} do
+      # Default test environment is not central mode
+      conn = get(conn, ~p"/")
+      assert redirected_to(conn) == "/app"
+    end
+
+    test "renders central page when in central mode", %{conn: conn} do
+      # Temporarily set deployment mode to central
+      original_mode = Application.get_env(:tau5, :deployment_mode)
+      Application.put_env(:tau5, :deployment_mode, :central)
+      
+      conn = get(conn, ~p"/")
+      response = html_response(conn, 200)
+      
+      # Check for key elements of the central page
+      assert response =~ "tau5.live"
+      assert response =~ "canvas"
+      assert response =~ "tau5-loading"
+      
+      # Restore original mode
+      Application.put_env(:tau5, :deployment_mode, original_mode)
+    end
+  end
+end
--- server/test/tau5_web/controllers/page_controller_test.exs
+++ server/test/tau5_web/controllers/page_controller_test.exs
@@ -1,8 +1,13 @@
 defmodule Tau5Web.PageControllerTest do
   use Tau5Web.ConnCase
 
-  test "GET /", %{conn: conn} do
+  test "GET / redirects to /app in non-central mode", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200)
+    assert redirected_to(conn) == "/app"
+  end
+
+  test "GET /app loads the main LiveView", %{conn: conn} do
+    conn = get(conn, ~p"/app")
+    assert html_response(conn, 200) =~ "phx-"
   end
 end
