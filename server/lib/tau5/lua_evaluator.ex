afe7b774bc65b757a52b8ff2d4eecc540086786d
Sam Aaron <samaaron@gmail.com>
2025-08-21 09:50:25 +0100

    Lua - give each lua eval max 10ms to execute
    Let's see if this runs into any issues further down the line.

--- server/lib/tau5/lua_evaluator.ex
+++ server/lib/tau5/lua_evaluator.ex
@@ -8,7 +8,7 @@ defmodule Tau5.LuaEvaluator do
   import :erlang, only: [spawn_opt: 2]
 
   @config %{
-    timeout_ms: 100,
+    timeout_ms: 10,
     max_heap_size: 1_000_000,
     max_output_size: 1_000_000
   }
