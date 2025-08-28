442930ab05d481639b2ac1709e1ec79a04341e01
Sam Aaron <samaaron@gmail.com>
2025-08-28 19:27:27 +0100

    bin - tau5-node.sh should only load dev builds

--- bin/mac/tau5-node.sh
+++ bin/mac/tau5-node.sh
@@ -5,13 +5,8 @@ GUI_DIR="${ROOT_DIR}/gui"
 BUILD_DIR="${GUI_DIR}/build"
 SERVER_DIR="${ROOT_DIR}/server"
 
-# Check if we have a Release build with tau5-node in the app bundle
-if [ -f "${ROOT_DIR}/Release/Tau5.app/Contents/MacOS/tau5-node" ]; then
-    # Release build - server is bundled in Resources
-    export TAU5_SERVER_PATH="${ROOT_DIR}/Release/Tau5.app/Contents/Resources"
-    "${ROOT_DIR}/Release/Tau5.app/Contents/MacOS/tau5-node" "$@"
-elif [ -f "${BUILD_DIR}/tau5-node" ]; then
-    # Development build - server is in the server directory
+# Development build only - Release is handled by the app itself
+if [ -f "${BUILD_DIR}/tau5-node" ]; then
     export TAU5_SERVER_PATH="${SERVER_DIR}"
     "${BUILD_DIR}/tau5-node" "$@"
 else
