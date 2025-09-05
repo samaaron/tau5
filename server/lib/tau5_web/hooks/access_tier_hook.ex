f536930b832f7defcdb1ebeb84471be25d123175
Sam Aaron <samaaron@gmail.com>
2025-09-05 21:27:21 +0100

    LiveView - fix socket detection in AccessTierHook

--- server/lib/tau5_web/hooks/access_tier_hook.ex
+++ server/lib/tau5_web/hooks/access_tier_hook.ex
@@ -9,21 +9,61 @@ defmodule Tau5Web.AccessTierHook do
   alias Tau5Web.AccessTier
   
   def on_mount(:default, _params, session, socket) do
-    # Create a pseudo-conn with friend auth status from session
-    pseudo_conn = if session["friend_authenticated"] do
-      %{assigns: %{friend_authenticated: true}}
-    else
-      nil
+    try do
+      require Logger
+      
+      if Application.get_env(:tau5, :dev_routes, false) do
+        Logger.debug("AccessTierHook: socket keys = #{inspect(Map.keys(socket))}")
+        Logger.debug("AccessTierHook: session keys = #{inspect(Map.keys(session))}")
+      end
+      
+      pseudo_conn = if session["friend_authenticated"] do
+        %{assigns: %{friend_authenticated: true}}
+      else
+        nil
+      end
+      
+      endpoint_module = cond do
+        is_map(socket) && Map.has_key?(socket, :endpoint) && 
+        not is_nil(Map.get(socket, :endpoint)) -> Map.get(socket, :endpoint)
+        session["endpoint"] == "public" -> Tau5Web.PublicEndpoint
+        true -> Tau5Web.Endpoint
+      end
+      
+      if Application.get_env(:tau5, :dev_routes, false) do
+        Logger.debug("AccessTierHook: detected endpoint_module = #{inspect(endpoint_module)}")
+      end
+      
+      {endpoint_type, access_tier, features} = 
+        AccessTier.get_access_info(endpoint_module, pseudo_conn)
+      
+      {:cont, 
+       socket
+       |> assign(:endpoint_type, endpoint_type)
+       |> assign(:access_tier, access_tier)
+       |> assign(:features, features)}
+    rescue
+      error ->
+        require Logger
+        Logger.error("AccessTierHook failed: #{inspect(error)}")
+        Logger.error("Socket structure: #{inspect(socket)}")
+        Logger.error("Session structure: #{inspect(session)}")
+        Logger.error("Stack: #{inspect(__STACKTRACE__)}")
+        
+        {:cont, 
+         socket
+         |> assign(:endpoint_type, "local")
+         |> assign(:access_tier, "full")
+         |> assign(:features, %{
+           admin_tools: true,
+           pairing: true,
+           fs_access: true,
+           mutate: true,
+           console_access: true,
+           lua_privileged: true,
+           midi_access: true,
+           link_access: true
+         })}
     end
-    
-    # Get the access tier using our pseudo conn
-    {endpoint_type, access_tier, features} = 
-      AccessTier.get_access_info(socket.endpoint, pseudo_conn)
-    
-    {:cont, 
-     socket
-     |> assign(:endpoint_type, endpoint_type)
-     |> assign(:access_tier, access_tier)
-     |> assign(:features, features)}
   end
 end
