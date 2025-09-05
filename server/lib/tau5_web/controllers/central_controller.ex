3bbfe716ce582f7b6a0a8b4248bbff3f93925172
Sam Aaron <samaaron@gmail.com>
2025-09-05 22:30:39 +0100

    Tests - fix central controller assigns structure

--- server/lib/tau5_web/controllers/central_controller.ex
+++ server/lib/tau5_web/controllers/central_controller.ex
@@ -4,13 +4,12 @@ defmodule Tau5Web.CentralController do
   def index(conn, _params) do
     case Application.get_env(:tau5, :deployment_mode) do
       :central ->
-        assigns = %{
+        render(conn, :index, 
           vertex_shader_path: ~p"/shaders/tau5-loading.vert",
           fragment_shader_path: ~p"/shaders/tau5-loading.frag",
           font_path: ~p"/fonts/CascadiaCodePL.woff2",
-          logo_path: ~p"/images/tau5-bw-hirez.png"
-        }
-        render(conn, :index, assigns: assigns, layout: false)
+          logo_path: ~p"/images/tau5-bw-hirez.png",
+          layout: false)
       _ ->
         redirect(conn, to: "/app")
     end
